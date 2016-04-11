42b812a998fb0bc497392a75859fd18df68560c6
Barack Obama <fa.de.haan@st.hanze.nl>
2016-04-11 17:42:21 +0200

    Line Graph

--- src/CarParkingSimulator/Model/CarQueue.java
+++ src/CarParkingSimulator/Model/CarQueue.java
@@ -3,17 +3,23 @@ package CarParkingSimulator.Model;
 import java.util.*;
 
 /**
- * Created by John on 29/03/16.
+ * Class for making a LinkedList which contains elements of Cars.
  */
 public class CarQueue
 {
     private Queue<Car> queue = new LinkedList<Car>();
 
+    /**
+     * This method appends the specified element to the end of this list.
+     */
     public boolean addCar(Car car)
     {
         return queue.add(car);
     }
 
+    /**
+     * This method retrieves and removes the head (first element) of this list.
+     */
     public Car removeCar()
     {
         return queue.poll();
